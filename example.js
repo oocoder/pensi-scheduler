f91ba884c3af1144c444ee15e855eb84bdcf8b76
Alex <calidadis@gmail.com>
2016-01-25 20:20:59 -0500

    updated example

--- example.js
+++ example.js
@@ -1,14 +1,16 @@
-// example.js 
+// EXAMPLE.JS - Sample use of pensi scheduler 
 var sched = require('./lib/pensi-scheduler');
 
-var TICKS_TWO_SECS = 2 * 1000;
+// Period to cycle scheduled events
+var ONE_DAY_PERIOD = 1000*60*60*24;
 
-// create a scheduler with one minute period.
-var sm = sched.create({period: TICKS_TWO_SECS});	
-sm.addTask('A', new Date(), {name : 'my task'}); // execute a minute from now.
+// Create a daily period scheduler. 
+var sm = sched.create({period: ONE_DAY_PERIOD});	
 
+// Run a task only once at 8a (local) time.
+sm.addTask('A', '08:00', {url: 'http://localhost:3000/_ah/start'}); 
 sm.once('interval', function(task){
-    console.log('Running Task', task.name, '...');
+    console.log('Sending request to ', task.meta.url);
 	sm.stop();
 });
 
